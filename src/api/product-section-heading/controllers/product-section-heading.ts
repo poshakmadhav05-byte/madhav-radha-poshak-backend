1540225e154af1142610d31e2199bf627e51bf22
Mohit Soni <mohitsoni522@gmail.com>
2026-09-17 21:32:04 +0530

    Initial commit - Strapi backend

---
+++ src/api/product-section-heading/controllers/product-section-heading.ts
@@ -0,0 +1,7 @@
+/**
+ * product-section-heading controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::product-section-heading.product-section-heading');
---
+++ src/api/product-section-heading/routes/product-section-heading.ts
@@ -0,0 +1,7 @@
+/**
+ * product-section-heading router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::product-section-heading.product-section-heading');
