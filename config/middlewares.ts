6d44ccc9bfee2ab95ade55d9f2850687bd493452
Mohit Soni <mohitsoni522@gmail.com>
2026-09-18 16:31:30 +0530

    Configure security middleware to allow Cloudinary image previews

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,7 +3,21 @@ import type { Core } from '@strapi/strapi';
 const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          // Yahan Cloudinary ka domain add kar diya gaya hai
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
